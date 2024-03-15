bb83670c0526214b7a832c0136f45b6f786e9e18
SaiLavanya1 <narthusailavanya10@gmail.com>
2024-03-15 13:54:25 -0700

    Reverse of vowels in a String

---
+++ reversevowels.java
@@ -0,0 +1,42 @@
+public class reversevowels {
+    // Return true if the character is a vowel (case-insensitive)
+    boolean isVowel(char c) {
+        return c == 'a' || c == 'i' || c == 'e' || c == 'o' || c == 'u'
+            || c == 'A' || c == 'I' || c == 'E' || c == 'O' || c == 'U';
+    }
+    // Function to reverse vowels in a string
+    public String reverseVowels(String s) {
+        char[] chars = s.toCharArray();       //converting string to array(o(n) ->space , due to string is immutable)
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            // Find the leftmost vowel
+            while (start < end && !isVowel(chars[start])) {
+                start++;
+            }
+            // Find the rightmost vowel
+            while (start < end && !isVowel(chars[end])) {
+                end--;
+            }
+            // Swap vowels
+            if (start < end) {
+                char temp = chars[start];
+                chars[start] = chars[end];
+                chars[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        // Convert char array back to String
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        reversevowels solution = new reversevowels();
+        String input = "hello world";
+        String reversed = solution.reverseVowels(input);
+        System.out.println(reversed);
+    }
+}
