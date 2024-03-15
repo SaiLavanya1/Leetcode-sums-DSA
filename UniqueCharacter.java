b4336ec5482201fbdb93632ccb03d10be6bb710f
SaiLavanya1 <narthusailavanya10@gmail.com>
2024-03-15 15:03:39 -0700

    Finding first unique character index of a string

---
+++ UniqueCharacter.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public class UniqueCharacter {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+        // build hash map : character and how often it appears
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);   //adding the frequency of elements in a hashmap
+        }
+        
+        // find the index
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1) 
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        UniqueCharacter solution = new UniqueCharacter();
+        // Test cases
+        String[] testCases = {"leetcode", "loveleetcode", "aabb"};
+        for (String testCase : testCases) {
+            int result = solution.firstUniqChar(testCase);
+            System.out.println("First unique character index in \"" + testCase + "\": " + result);
+        }
+    }
+}
