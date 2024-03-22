b0689fd99e806470cd683dbbd949894c3c4cf320
SaiLavanya1 <narthusailavanya10@gmail.com>
2024-03-22 14:55:18 -0700

    TwoSum Problem

---
+++ TwoSum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] indices = twoSum(nums, target);
+        System.out.println("Indices of the two numbers: " + Arrays.toString(indices));
+    }
+}
+
+
+
+
+////Time Complexity: O(n)     
+//Space Complexity: O(n)
