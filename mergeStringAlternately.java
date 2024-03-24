d9a295f0c8065e23e5a8df1b7feb1b3596b6a6e8
Sailavanya Narthu <98532973+SaiLavanya1@users.noreply.github.com>
2024-03-24 00:23:50 -0700

    Update mergeStringAlternately.java

--- mergeStringAlternately.java
+++ mergeStringAlternately.java
@@ -17,3 +17,26 @@ class Solution {
 
 //Time - O(m+n) -> m for word1, n for word2
 //Space - O(1)
+
+Two Pointer Solution:
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+  int m = word1.length();
+        int n = word2.length();
+        StringBuilder result = new StringBuilder();
+        int i = 0, j = 0;
+
+        while (i < m || j < n) {
+            if (i < m) {
+                result.append(word1.charAt(i));
+		i++;
+            }
+            if (j < n) {
+                result.append(word2.charAt(j));
+		j++
+            }
+        }
+
+        return result.toString();
+}
+}
