af5f435c05a7ede539f179ccbfd700888eb39b3e
Sailavanya Narthu <98532973+SaiLavanya1@users.noreply.github.com>
2024-03-25 16:05:26 -0700

    Add files via upload

---
+++ increasingTriplet.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first_num = Integer.MAX_VALUE;
+        int second_num = Integer.MAX_VALUE;
+        for(int n : nums){
+            if(n<=first_num)
+            first_num=n;
+            else if(n<=second_num)
+            second_num=n;
+            else return true;
+        }
+        return false;
+    }
+ }
+ //Time - O(N)
+ //Space -O(1)
+ 
+ 
+ 
