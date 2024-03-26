c68f4f31afe284fdfb91b00077ab6d9b8c81e67f
Sailavanya Narthu <98532973+SaiLavanya1@users.noreply.github.com>
2024-03-25 18:36:17 -0700

    Add files via upload

---
+++ longestPalindrome.java
@@ -0,0 +1,52 @@
+//Longest Plaindrome Substring
+class Solution {
+
+    String longestPalindrome(String str) {
+  
+      if (str.length() <= 1)
+        return str;
+  
+      String LPS = "";
+  
+      for (int i = 1; i < str.length(); i++) {
+  
+        // Consider odd length
+        int low = i;
+        int high = i;
+        while(str.charAt(low) == str.charAt(high)) {
+          low--;
+          high++;
+  
+          if (low == -1 || high == str.length())
+            break;
+        }
+  
+        String palindrome = str.substring(low+1, high);
+        if (palindrome.length() > LPS.length()) {
+          LPS = palindrome;
+        }
+  
+        // Consider even length
+        low = i-1;
+        high = i;
+        while(str.charAt(low) == str.charAt(high)) {
+          low--;
+          high++;
+  
+          if (low == -1 || high == str.length())
+            break;
+        }
+  
+        palindrome = str.substring(low+1, high);
+        if (palindrome.length() > LPS.length()) {
+          LPS = palindrome;
+        }
+      }
+  
+      return LPS;
+    }
+  
+  }
+  
+  // Time complexity - O(n^2) using for even and odd
+  //Space complexity - O(1)
