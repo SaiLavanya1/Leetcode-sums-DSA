3d14451fd9dad41166a040f4b5f086f0fcf977b8
Sailavanya Narthu <98532973+SaiLavanya1@users.noreply.github.com>
2024-03-26 17:39:48 -0700

    Add files via upload

---
+++ maxOperations.java
@@ -0,0 +1,25 @@
+//Max number of K-sum pairs
+import java.util.*;
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int count = 0;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] < k) {
+                left++;
+            } else if (nums[left] + nums[right] > k) {
+                right--;
+            } else {
+                left++;
+                right--;
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+//Time - O(nlogn) -> for sorting o(nlogn) and for traversing o(n)
+//Space - O(1)
